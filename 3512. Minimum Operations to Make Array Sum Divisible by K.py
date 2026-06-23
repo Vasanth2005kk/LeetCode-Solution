a05549c5013dbaeeb3d6439358b35d74f6899a19
Vasanth2005kk <vasanth2005kk@gmail.com>
2026-06-23 17:49:49 +0530

    3512. Minimum Operations to Make Array Sum Divisible by K.py

---
+++ 3512. Minimum Operations to Make Array Sum Divisible by K.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        return sum(nums)%k
+
+nums = [3,9,7]
+k = 5
+
+obj = Solution().minOperations(nums,k)
+
+print(obj)
