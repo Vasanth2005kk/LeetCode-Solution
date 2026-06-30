f10c749221a96d5fa855a70ce38593da56978695
Vasanth2005kk <vasanth2005kk@gmail.com>
2026-06-30 12:41:37 +0530

    217. Contains Duplicate.py

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -10,3 +10,16 @@ class Solution:
 obj = Solution().containsDuplicate(nums)
 
 print(obj)
+
+
+"""
+class Solution:
+    def containsDuplicate(self, nums):        
+        s=set()
+        for i in nums:
+            if i not in s:
+                s.add(i)
+            else:
+                return True
+        return False
+"""
